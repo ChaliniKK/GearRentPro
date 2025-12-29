8ee137e0498a591719b156a79b66165c6968b44f
Chalini Kaushalya <chalinikaushalya14@gmail.com>
2025-12-29 21:41:59 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,1 @@
-import dao.ReportDAO;
 
-public class Main {
-    public static void main(String[] args) {
-
-        ReportDAO reportDAO = new ReportDAO();
-        reportDAO.getEquipmentUtilizationReport(1);
-    }
-}
